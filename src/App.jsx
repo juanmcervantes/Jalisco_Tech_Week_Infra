803650c97cb3abe6614531d8b40c25288626ae04
Juan Cervantes <juancervantes0019@gmail.com>
2025-07-05 22:32:42 -0600

    components folder created

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import * as React from "react"
 
 function App() {
   return (
+    <>
     <div className="min-h-screen flex items-center justify-center bg-base-200">
       <button className="btn btn-primary">¡Botón DaisyUI!</button>
     </div>
+    </>
   )
 
 }
