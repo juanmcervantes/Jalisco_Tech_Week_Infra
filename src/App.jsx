3c40c52f4195906cdcf183b3fee623080536cd6a
Juan Cervantes <juancervantes0019@gmail.com>
2025-07-05 22:42:55 -0600

    Navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import * as React from "react"
-
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
+    <Navbar/>
     <div className="min-h-screen flex items-center justify-center bg-base-200">
       <button className="btn btn-primary">¡Botón DaisyUI!</button>
     </div>
