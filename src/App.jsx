9bc7c418910e58d2a079fc98353aed21a965fa65
Juan Cervantes <juancervantes0019@gmail.com>
2025-07-07 18:15:21 -0600

    HeadingCounter added above the Condown component for styling reasons

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Footer from './components/Footer'
 import MainHero from './components/MainHero'
 import FormButton from './components/FormButton'
 import Countdown from './components/Countdown'
+import HeadingCounter from "./components/HeadingCounter"
+
 
 function App() {
   return (
     <>
     <MainNavbar/>
     <MainHero/>
+    <HeadingCounter/>
     <Countdown/>
     <FormButton/>
     <Footer/>
---
+++ src/components/HeadingCounter.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+function HeadingCounter() {
+  return (
+    <div className="w-full flex justify-center items-center bg-[#0e1a24] px-4 pt-10">
+      <h2 className="text-cyan-400 font-extrabold text-3xl sm:text-4xl md:text-5xl uppercase tracking-wider text-center animate-text-fade">
+        ¡Ya solo faltan!
+      </h2>
+    </div>
+  )
+}
+
+export default HeadingCounter
